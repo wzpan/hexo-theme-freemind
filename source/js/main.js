2ff419ea6aaa2a75caf81ee1252c9d8296adc06d
Joseph Pan <cs.wzpan@gmail.com>
2014-03-23 11:51:21 +0000

    Use jQuery fading effect to fix issue #1.

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
 
-    $(window).scroll(function(){  //只要窗口滚动,就触发下面代码 
+	$(window).scroll(function(){  //只要窗口滚动,就触发下面代码 
         var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度 
-        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示  
-            $("#gotop").fadeIn(400); //淡出     
-        }else{      
-            $("#gotop").stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动   
+        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示
+            $("#gotop").fadeIn(400); //淡出
+			$(".navbar").stop().fadeTo(400, 0.2);
+        }else{
+            $("#gotop").fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动
+			$(".navbar").stop().fadeTo(400, 1);
         }
     });
     $("#gotop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
         $("html,body").animate({scrollTop:"0px"},200);
     });
+	$(".navbar").hover(function(){
+		$(".navbar").fadeTo(400, 1);
+	});
 
 });
