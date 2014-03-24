ce5155b441c8c932b0a304436660014815ec68c6
Joseph Pan <cs.wzpan@gmail.com>
2014-03-24 19:48:23 +0000

    Fade out the navbar when mouse leave.

--- source/js/main.js
+++ source/js/main.js
@@ -13,8 +13,13 @@ $(document).ready(function() {
     $("#gotop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
         $("html,body").animate({scrollTop:"0px"},200);
     });
-	$(".navbar").hover(function(){
-		$(".navbar").fadeTo(400, 1);
+	$(".navbar").mouseenter(function(){
+		$(".navbar").fadeTo(100, 1);
 	});
-
+    $(".navbar").mouseleave(function(){
+		var scrollt = document.documentElement.scrollTop + document.body.scrollTop;
+		if ( scrollt > 200) {
+			$(".navbar").fadeTo(100, 0.2);
+		}
+	});
 });
