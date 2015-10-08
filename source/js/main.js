83a87ad95c6fe6bfbbc649e8608017d4c1997168
Joseph Pan <cs.wzpan@gmail.com>
2015-10-08 12:10:05 +0800

    Details improved.

--- source/js/main.js
+++ source/js/main.js
@@ -36,13 +36,19 @@ $(document).ready(function() {
 replaceMeta = function(){
 	if ($(window).width() < 980) {
 		if ($("#side_meta #post_meta").length>0) {
-			console.log("side has post meta");
 			$("#post_meta").appendTo("#top_meta");
+		}
+		if ($("#sidebar #site_search").length>0) {
+			$("#site_search").appendTo("#top_search");
+			$("#site_search #st-search-input").css("width", "95%");
 		}
 	} else {
 		if ($("#top_meta #post_meta").length>0) {
-			console.log("top has post meta");
 			$("#post_meta").appendTo("#side_meta");
+		}
+		if ($("#top_search #site_search").length>0) {
+			$("#site_search").prependTo("#sidebar");
+			$("#site_search #st-search-input").css("width", "80%");
 		}
 	}
 }
